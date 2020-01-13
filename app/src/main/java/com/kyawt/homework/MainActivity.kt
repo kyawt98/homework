d4780b1c8081cdb136f5dd9645daf93aa3b4fe39
Your Name <kyawtkyawtsan1998@gmail.com>
2020-01-13 15:49:23 +0630

    commit for Homework

---
+++ app/src/main/java/com/kyawt/homework/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kyawt.homework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.homework3)
+    }
+}
